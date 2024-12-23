1fa6f61a2d5f55f9b7c12e4650db375fd8219bbd
Siva Rama Krishna Prasad Changala <123384127+cleancoder1016@users.noreply.github.com>
2024-12-23 21:10:32 +0000

    added new

--- Serendipity/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Serendipity/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0595e7ceba6668e354f6d1f97df3e4492311a653")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a28a51cad0038c215a5d2e2775fec2d04317fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
